5bb5cf72a04583b5a0627259bc4f96dd97e48da4
Yenthe Oerlemans <yentheoerlemans@gmail.com>
2022-09-15 19:45:44 +0200

    just make it easier for me and use the minecraft add to inventory functionality itself

--- spigot/src/main/java/one/spectra/Sorter.java
+++ spigot/src/main/java/one/spectra/Sorter.java
@@ -19,7 +19,6 @@ public class Sorter {
         // skip hotbar, start at 9
         for (int i = 9; i < 36; i++) {
             ItemStack stack = playerInventory.getItem(i);
-
             playerInventory.clear(i);
             if (stack != null && !stack.getType().isAir()) {
                 tempInventory.add(stack);
@@ -30,10 +29,10 @@ public class Sorter {
 
         comparator = comparator.thenComparing(countComparison.reversed());
 
-        var merged = MergeStacks(tempInventory).stream().sorted(comparator).toList();
+        var merged = tempInventory.stream().sorted(comparator).toList();
 
-        for (var i = 0; i < merged.size(); i++) {
-            playerInventory.setItem(i + 9, merged.get(i));
+        for (ItemStack stack : merged) {
+            playerInventory.addItem(stack);
         }
     }
 
@@ -56,45 +55,10 @@ public class Sorter {
 
         comparator = comparator.thenComparing(countComparison.reversed());
 
-        var merged = MergeStacks(tempInventory).stream().sorted(comparator).toList();
+        var merged = tempInventory.stream().sorted(comparator).toList();
 
-        for (var i = 0; i < merged.size(); i++) {
-            inventory.setItem(i, merged.get(i));
+        for (ItemStack stack : merged) {
+            inventory.addItem(stack);
         }
-    }
-
-    private ArrayList<ItemStack> MergeStacks(ArrayList<ItemStack> original) {
-        var merged = new ArrayList<ItemStack>();
-        for (ItemStack itemStack : original) {
-            if (IsEligibleForMerging(itemStack)) {
-                var optionalFirstFreeStack = merged.stream().filter(x -> IsEligibleForMerging(x))
-                        .filter(x -> x.getType().compareTo(itemStack.getType()) == 0).findFirst();
-                if (optionalFirstFreeStack.isPresent()) {
-                    var firstFreeStack = optionalFirstFreeStack.get();
-                    var currentAmount = firstFreeStack.getAmount();
-                    var amountToAdd = itemStack.getAmount();
-                    var newAmount = currentAmount + amountToAdd;
-                    var overflow = newAmount - itemStack.getMaxStackSize();
-                    if (overflow > 0) {
-                        firstFreeStack.setAmount(itemStack.getMaxStackSize());
-                        itemStack.setAmount(overflow);
-                        merged.add(itemStack);
-                    } else {
-                        firstFreeStack.setAmount(newAmount);
-                    }
-                } else {
-                    merged.add(itemStack);
-                }
-            } else {
-                merged.add(itemStack);
-            }
-        }
-        return merged;
-    }
-
-    private boolean IsEligibleForMerging(ItemStack itemStack) {
-        return itemStack.getMaxStackSize() > 1
-                && !itemStack.getItemMeta().hasDisplayName()
-                && itemStack.getType() != Material.PLAYER_HEAD;
     }
 }
