76060498c58e4f5a12000b586cd511715e644b82
YentheO <unconfigured@null.spigotmc.org>
2023-09-08 23:46:49 +0200

    changed image

--- forge/src/main/java/one/spectra/better_chests/message_handlers/ConfigureCurrentChestRequestHandler.java
+++ forge/src/main/java/one/spectra/better_chests/message_handlers/ConfigureCurrentChestRequestHandler.java
@@ -1,15 +1,28 @@
 package one.spectra.better_chests.message_handlers;
 
+import java.util.logging.Logger;
+
+import com.google.inject.Inject;
+
 import one.spectra.better_chests.abstractions.Player;
 import one.spectra.better_chests.message_handlers.messages.ConfigureCurrentChestRequest;
 
 public class ConfigureCurrentChestRequestHandler implements MessageHandler<ConfigureCurrentChestRequest> {
 
+    private Logger _logger;
+
+    @Inject
+    public ConfigureCurrentChestRequestHandler(Logger logger) {
+        _logger = logger;
+    }
+
     @Override
     public void handle(Player player, ConfigureCurrentChestRequest message) {
+        _logger.info("Setting config");
         var openInventory = player.getOpenContainer();
         if (openInventory != null) {
             openInventory.setSpread(message.configuration.spread);
+            _logger.info("Set config");
         }
     }
 }
